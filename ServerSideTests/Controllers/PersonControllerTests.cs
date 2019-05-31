1f6d13d853f663c9b3d7b3bb9213689fa1f217c6
David Coyer <david@coyer.net>
2019-05-31 12:18:04 -0500

    Sample Ready

--- ServerSideTests/Controllers/PersonControllerTests.cs
+++ ServerSideTests/Controllers/PersonControllerTests.cs
@@ -1,18 +1,35 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using NUnit.Framework;
+using ServerSide.Controllers;
+using ServerSide.Models;
+using ServerSide.Services;
 
 namespace Tests
 {
-    public class Tests
+    public class PersonControllerTests
     {
+        private Mock<IPersonService> _personService = new Mock<IPersonService>();
+        private PersonController _personController;
+
         [SetUp]
         public void Setup()
         {
+            _personController = new PersonController(_personService.Object);
         }
 
         [Test]
-        public void Test1()
+        public void FindAllReturnsListofPerson()
         {
-            Assert.Pass();
+            List<Person> personList = new List<Person> { new Person { Id = 1 } };
+            _personService.Setup(p => p.findAll()).Returns(personList);
+
+            var controllerReturn = _personController.findAll().Result as ObjectResult;
+            var returnedList = controllerReturn.Value as List<Person>;
+
+            Assert.AreEqual(200, controllerReturn.StatusCode.Value);
+            Assert.AreEqual(1, returnedList.Count);
         }
     }
 }
